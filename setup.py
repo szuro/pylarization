f0ff101b66d7f81a9c45af8d2d645ea877e35854
Robert Szulist <r.szulist@gmail.com>
2018-01-25 14:47:49 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pylarization',
+    version='0.0.1',
+    description='Package for polarization state calculations',
+    long_description=long_description,
+    url='https://github.com/szuro/pylarization',
+    author='Robert Szulist',
+    author_email='r.szulist@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='polarization ellipse jones stokes',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['numpy'],
+)
