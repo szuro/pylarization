259ff6d6b2d49d384bb21ba7e9ca57d38e0ae1e6
Robert Szulist <r.szulist@gmail.com>
2018-01-26 22:23:13 +0100

    Add basic variables in Elllipse class
    All marked as private - values will be stored in radians.
    More often than not the user will want to use degrees.
    A convinient getter will convert the values.

--- pylarization.py
+++ pylarization.py
@@ -6,8 +6,16 @@ Module containing classes used for describing light polarization.
 class Ellipse(object):
     """
     Class for describing polarization state using trygonometry.
+    _ellipticity_angle - assuming A is equal to half of the major axis and
+        B is equal to half of the minor axis then the tangent of
+        ellipticity_angle is equal to B/A.
+    _azimuth - angle between the major axis of the ellipse and the x-axis.
+    _phase - the phase difference between x and y components.
     """
-    pass
+    def __init__(self):
+        self._ellipticity_angle = 0
+        self._azimuth = 0
+        self._phase = 0
 
 
 class Jones(Ellipse):
