ad1ec9d829a262a79917880b9a595fd6bcf64023
Robert Szulist <r.szulist@gmail.com>
2018-01-26 11:52:50 +0100

    Add pylarization module with class stubs
    Elliptic is the base class for both Jones and Stokes, because they are a
    method of presenting the same data in a more compact way.
    It should be possible to calculate all trygonometric properties from
    Jones and Stokes.

---
+++ pylarization.py
@@ -0,0 +1,24 @@
+"""
+Module containing classes used for describing light polarization.
+"""
+
+
+class Elliptic(object):
+    """
+    Class for describing polarization state using trygonometry.
+    """
+    pass
+
+
+class Jones(Elliptic):
+    """
+    Class for describing polarization state using Jones vector.
+    """
+    pass
+
+
+class Stokes(Elliptic):
+    """
+    Class for describing polarization state using Stokes vector.
+    """
+    pass
