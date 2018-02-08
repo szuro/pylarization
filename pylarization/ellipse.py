6086e2de51c17743f8e9e97c9bf7f909b189f814
Robert Szulist <r.szulist@gmail.com>
2018-02-08 21:37:31 +0100

    Add documentation

--- pylarization/ellipse.py
+++ pylarization/ellipse.py
@@ -7,12 +7,17 @@ import numpy as np
 class PolariaztionEllipse(object):
     """
     Class for describing polarization state using trygonometry.
+    _amplitudes - float matrix holding values of x and y amplitudes of
+        electric field vector.
     _ellipticity_angle - assuming A is equal to half of the major axis and
         B is equal to half of the minor axis then the tangent of
-        ellipticity_angle is equal to B/A.
+        ellipticity_angle is equal to B/A. After removing denominators
+        it's obvious that it's just a ratio of the two axes.
     _azimuth - angle between the major axis of the ellipse and the x-axis.
     _phase - the phase difference between x and y components.
     _diagonal_angle - ratio between amplitudes of light.
+    _minor_axis - lenght of the minor axis of polarization ellipse.
+    _major_axis -  lenght of the major axis of polarization ellipse.
     """
     def __init__(self, **kwargs):
         self._amplitudes = np.matrix([[0], [0]], dtype=float)
@@ -74,35 +79,58 @@ class PolariaztionEllipse(object):
         self._major_axis = E0 * np.sqrt(1 + sqrt_in_numerator) * np.sqrt(0.5)
 
     def _calc_complement_diagonal_angle(self):
+        """
+        Calculate the complement of diagonal angle.
+        """
         return np.pi / 2 - self._diagonal_angle
 
     def _calc_azimuth(self):
+        """
+        Calculate the azimuth of polarization ellipse.
+        """
         self._azimuth = 0.5 * np.arctan(
             np.tan(2 * self._diagonal_angle) * np.cos(self._phase)
             )
 
     def _calc_ellipticity_angle(self):
+        """
+        Calculate the ellipticity angle of polarization ellipse.
+        """
         self._ellipticity_angle = 0.5 * np.arcsin(
             np.tan(2 * self._diagonal_angle) * np.sin(self._phase)
             )
 
     def _calc_diagonal_angle_from_amplitudes(self):
+        """
+        Calculate the diagtonal angle of polarization ellipse using amplitudes.
+        """
         self._diagonal_angle = np.arctan(
             self._amplitudes[1],
             self._amplitudes[0]
             )
 
     def _calc_diagonal_angle_from_azimuth(self):
+        """
+        Calculate the diagtonal angle of polarization ellipse using azumuth
+        and phase diference.
+        """
         self._diagonal_angle = 0.5 * np.arctan(
             np.tan(2 * self._azimuth) / np.cos(self._phase)
             )
 
     def _calc_diagonal_angle_from_ellipticity_angle(self):
+        """
+        Calculate the diagtonal angle of polarization ellipse using
+        ellipticity angle and phase diference.
+        """
         self._diagonal_angle = 0.5 * np.arcsin(
             np.sin(2 * self._ellipticity_angle) / np.sin(self._phase)
             )
 
     def _calc_phase(self):
+        """
+        Calculate the phase difference.
+        """
         self._phase = np.arctan(
             np.tan(2 * self._ellipticity_angle) /
             np.sin(2 * self._azimuth)
