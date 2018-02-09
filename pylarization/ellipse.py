82fb7949a5ad9c39ce8b531e2c11d59d045fe209
Robert Szulist <r.szulist@gmail.com>
2018-02-09 22:34:57 +0100

    Rework PolarizationEllipse class to use properties
    It is much more convinient to use properties instead of trying to call
    lots of methods in __init__. It also helps to improve readability.
    This commit also fixes a typo in the class name.

--- pylarization/ellipse.py
+++ pylarization/ellipse.py
@@ -4,134 +4,130 @@ Module containing a class describing polarization state using trigonometry.
 import numpy as np
 
 
-class PolariaztionEllipse(object):
+class PolarizationEllipse(object):
     """
     Class for describing polarization state using trygonometry.
-    _amplitudes - float matrix holding values of x and y amplitudes of
+    amplitudes - float matrix holding values of x and y amplitudes of
         electric field vector.
-    _ellipticity_angle - assuming A is equal to half of the major axis and
+    ellipticity_angle - assuming A is equal to half of the major axis and
         B is equal to half of the minor axis then the tangent of
         ellipticity_angle is equal to B/A. After removing denominators
         it's obvious that it's just a ratio of the two axes.
-    _azimuth - angle between the major axis of the ellipse and the x-axis.
-    _phase - the phase difference between x and y components.
-    _diagonal_angle - ratio between amplitudes of light.
-    _minor_axis - lenght of the minor axis of polarization ellipse.
-    _major_axis -  lenght of the major axis of polarization ellipse.
+    azimuth - angle between the major axis of the ellipse and the x-axis.
+    phase - the phase difference between x and y components.
+    diagonal_angle - ratio between amplitudes of light.
+    complement_diagonal_angle - the complement of the diagonal angle.
     """
-    def __init__(self, **kwargs):
-        self._amplitudes = np.matrix([[0], [0]], dtype=float)
-        self._amplitudes[0] = kwargs.get('E0x', 0.0)
-        self._amplitudes[1] = kwargs.get('E0y', 0.0)
-        self._ellipticity_angle = kwargs.get('ellipticity_angle')
-        self._azimuth = kwargs.get('azimuth')
-        self._phase = kwargs.get('phase')
-        self._diagonal_angle = kwargs.get('diagonal_angle')
-        self._minor_axis = 0
-        self._major_axis = 0
+    def __init__(self, E0x, E0y, phase):
+        self._amplitudes = np.matrix([[E0x], [E0y]], dtype=float)
+        self._phase = phase
 
-    def _get_property(self, name, degrees=False):
+    @property
+    def amplitudes(self):
         """
-        Return given property in degrees or radians.
+        Return the amplitudes of the ellipse.
         """
-        if degrees:
-            return np.rad2deg(name)
-        return name
+        return self._amplitudes[0].item(), self._amplitudes[1].item()
 
-    def get_phase(self, degrees=False):
+    @property
+    def phase(self):
         """
         Return the phase difference of light amplitudes.
-        Degrees are optional as numpy uses radians internally.
         """
-        return self._get_property(self._phase, degrees)
+        return self._phase
 
-    def get_azimuth(self, degrees=False):
+    @property
+    def azimuth(self):
         """
         Return the azimuth property of light polarization.
-        Degrees are optional as numpy uses radians internally.
+        Values should be in range:
+        -diagonal angle <= azimuth <= diagonal angle
         """
-        return self._get_property(self._azimuth, degrees)
+        azimuth = 0.5 * np.arctan(
+            np.tan(2 * self.diagonal_angle) * np.cos(self._phase)
+            )
+        return azimuth
 
-    def get_ellipticity_angle(self, degrees=False):
+    @property
+    def ellipticity_angle(self, degrees=False):
         """
         Return the ratio of minor to major ellipse axis.
-        Degrees are optional as numpy uses radians internally.
+        Values should be in range:
+        -pi/4 <= ellipticity angle <= pi/4
         """
-        return self._get_property(self._ellipticity_angle, degrees)
+        ellipticity_angle = 0.5 * np.arcsin(
+            np.tan(2 * self.diagonal_angle) * np.sin(self._phase)
+            )
+        return ellipticity_angle
 
-    def get_diagonal_angle(self, degrees=False):
+    @property
+    def diagonal_angle(self, degrees=False):
         """
         Return the diagonal angle of th rectangle created by light amplitudes.
-        Degrees are optional as numpy uses radians internally.
+        Values should be in range:
+        0 <= diagonal angle <= pi/2
         """
-        return self._get_property(self._diagonal_angle, degrees)
+        diagonal_angle = np.arctan2(
+            self._amplitudes[1].item(),
+            self._amplitudes[0].item()
+            )
+        return diagonal_angle
 
-    def _calc_axes(self):
+    @property
+    def complement_diagonal_angle(self):
+        """
+        Calculate the complement of diagonal angle.
+        Values should be in range:
+        0 <= complement of diagonal angle <= pi/2
+        """
+        return np.pi / 2 - self.diagonal_angle
+
+    def calc_axes(self):
         """
         Calculate both axes of the ellipse.
         """
         E0 = np.square(self._amplitudes).sum()
-        compelent = self._calc_complement_diagonal_angle()
+        compelent = self.complement_diagonal_angle()
         sqrt_in_numerator = np.sqrt(1 - np.square(np.sin(compelent)) *
                                     np.square(np.sin(self._phase))
                                     )
-        self._minor_axis = E0 * np.sqrt(1 - sqrt_in_numerator) * np.sqrt(0.5)
-        self._major_axis = E0 * np.sqrt(1 + sqrt_in_numerator) * np.sqrt(0.5)
-
-    def _calc_complement_diagonal_angle(self):
-        """
-        Calculate the complement of diagonal angle.
-        """
-        return np.pi / 2 - self._diagonal_angle
-
-    def _calc_azimuth(self):
-        """
-        Calculate the azimuth of polarization ellipse.
-        """
-        self._azimuth = 0.5 * np.arctan(
-            np.tan(2 * self._diagonal_angle) * np.cos(self._phase)
-            )
-
-    def _calc_ellipticity_angle(self):
-        """
-        Calculate the ellipticity angle of polarization ellipse.
-        """
-        self._ellipticity_angle = 0.5 * np.arcsin(
-            np.tan(2 * self._diagonal_angle) * np.sin(self._phase)
-            )
-
-    def _calc_diagonal_angle_from_amplitudes(self):
-        """
-        Calculate the diagtonal angle of polarization ellipse using amplitudes.
-        """
-        self._diagonal_angle = np.arctan(
-            self._amplitudes[1],
-            self._amplitudes[0]
-            )
+        minor_axis = E0 * np.sqrt(1 - sqrt_in_numerator) * np.sqrt(0.5)
+        major_axis = E0 * np.sqrt(1 + sqrt_in_numerator) * np.sqrt(0.5)
+        return major_axis, minor_axis
 
     def _calc_diagonal_angle_from_azimuth(self):
         """
         Calculate the diagtonal angle of polarization ellipse using azumuth
         and phase diference.
         """
-        self._diagonal_angle = 0.5 * np.arctan(
-            np.tan(2 * self._azimuth) / np.cos(self._phase)
+        diagonal_angle = 0.5 * np.arctan(
+            np.tan(2 * self.azimuth) / np.cos(self._phase)
             )
+        return diagonal_angle
 
     def _calc_diagonal_angle_from_ellipticity_angle(self):
         """
         Calculate the diagtonal angle of polarization ellipse using
         ellipticity angle and phase diference.
         """
-        self._diagonal_angle = 0.5 * np.arcsin(
-            np.sin(2 * self._ellipticity_angle) / np.sin(self._phase)
+        diagonal_angle = 0.5 * np.arcsin(
+            np.sin(2 * self.ellipticity_angle) / np.sin(self._phase)
             )
+        return diagonal_angle
 
     def _calc_phase(self):
         """
         Calculate the phase difference.
         """
-        self._phase = np.arctan(
-            np.tan(2 * self._ellipticity_angle) /
-            np.sin(2 * self._azimuth)
+        phase = np.arctan(
+            np.tan(2 * self.ellipticity_angle) /
+            np.sin(2 * self.azimuth)
+            )
+        return phase
+
+    def __str__(self):
+        return "E0x={}, E0y={}, phase={}".format(
+            self._amplitudes[0].item(),
+            self._amplitudes[0].item(),
+            self._phase
             )
--- pylarization/vectors.py
+++ pylarization/vectors.py
@@ -2,17 +2,17 @@
 Module containing classes describing Jones and Stokes vectors.
 Both are used in describing light polarization.
 """
-from pylarization.ellipse import PolariaztionEllipse
+from pylarization.ellipse import PolarizationEllipse
 
 
-class JonesVector(PolariaztionEllipse):
+class JonesVector(PolarizationEllipse):
     """
     Class for describing polarization state using Jones vector.
     """
     pass
 
 
-class StokesVector(PolariaztionEllipse):
+class StokesVector(PolarizationEllipse):
     """
     Class for describing polarization state using Stokes vector.
     """
