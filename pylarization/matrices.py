99ff354fbff4404a3c3cdb2d6976acd946c7262f
Robert Szulist <r.szulist@gmail.com>
2018-02-23 22:44:59 +0100

    Create classes stubs

---
+++ pylarization/matrices.py
@@ -0,0 +1,19 @@
+"""
+Module containing classes describing Jones and Mueller matrices.
+Both are used in transforming light polarization states.
+It also contains the coherency matrix, which is used to describe polarization.
+"""
+import numpy as np
+from vectors import JonesVector, StokesVector
+
+
+class JonesMatrix(object):
+    pass
+
+
+class MuellerMatrix(object):
+    pass
+
+
+class CoherencyMatrix(object):
+    pass
