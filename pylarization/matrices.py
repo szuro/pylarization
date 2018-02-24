b8178912eb51af3c4094789051c0cb602979abfe
Robert Szulist <r.szulist@gmail.com>
2018-02-24 09:23:48 +0100

    Improve JonenMatrix __mul__
    Multiplication now differences between a Vector and a Matrix.
    This removes a ton of paranthesis when multiplying many elements.

--- pylarization/matrices.py
+++ pylarization/matrices.py
@@ -11,10 +11,13 @@ class JonesMatrix(object):
     def __init__(self, a11, a12, a21, a22):
         self._matrix = np.matrix([[a11, a12], [a21, a22]], dtype=complex)
 
-    def __mul__(self, jones):
-        product = self._matrix * jones.vector
-        new_jones = JonesVector(product[0].item(), product[1].item())
-        return new_jones
+    def __mul__(self, other):
+        if isinstance(other, JonesVector):
+            product = self._matrix * other.vector
+            return JonesVector(product[0].item(), product[1].item())
+        elif isinstance(other, JonesMatrix):
+            product = self._matrix * other.matrix
+            return JonesMatrix.from_matrix(product)
 
     def __rmul__(self, jones):
         raise ValueError("Wrong operation order")
@@ -22,6 +25,16 @@ class JonesMatrix(object):
     @classmethod
     def from_list(cls, list_):
         return cls(list_[0], list_[1], list_[2], list_[3])
+
+    @classmethod
+    def from_matrix(cls, matrix_):
+        a11, a12 = matrix_.tolist()[0]
+        a21, a22 = matrix_.tolist()[1]
+        return cls(a11, a12, a21, a22)
+
+    @property
+    def matrix(self):
+        return self._matrix
 
 
 class MuellerMatrix(object):
