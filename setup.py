45ef25ca59d65982fad269e4559ddf75c2bf34c4
Robert Szulist <r.szulist@gmail.com>
2018-03-10 22:22:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pylarization',
-    version='0.0.5',
+    version='0.0.6',
     description='Package for polarization state calculations',
     long_description=long_description,
     url='https://github.com/szuro/pylarization',
