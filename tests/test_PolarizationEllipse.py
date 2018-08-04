0edecce63aee410afb5e7e74fe0666ac0ce19374
Robert Szulist <r.szulist@gmail.com>
2018-08-04 15:41:10 +0200

    Add tests for PolarizationEllipse

---
+++ tests/test_PolarizationEllipse.py
@@ -0,0 +1,18 @@
+import unittest
+from math import sqrt, pi
+import math
+from pylarization.ellipse import PolarizationEllipse
+from tests import DummyVector
+
+
+class PolarizationEllipseValues(DummyVector.DummyVectorValues):
+    def setUp(self):
+        self.linear_horizontal = PolarizationEllipse(1.0, 0.0, 0.0)
+        self.linear_vertical = PolarizationEllipse(0.0, 1.0, 0.0)
+        self.linear_plus45 = PolarizationEllipse(0.7071068, 0.7071068, 0.0)
+        self.linear_minus45 = PolarizationEllipse(0.7071068, -0.7071068, 0.0)
+        self.elliptic = PolarizationEllipse(0.445, 0.89, pi/2)
+
+
+if __name__ == '__main__':
+    unittest.main()
