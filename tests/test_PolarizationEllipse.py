e21cf6a6595677160dcadcdf259f086f1a3395b9
Robert Szulist <r.szulist@gmail.com>
2018-08-04 20:38:36 +0200

    Add missing test cases

--- tests/test_PolarizationEllipse.py
+++ tests/test_PolarizationEllipse.py
@@ -12,6 +12,8 @@ class PolarizationEllipseValues(DummyVector.DummyVectorValues):
         self.linear_plus45 = PolarizationEllipse(0.7071068, 0.7071068, 0.0)
         self.linear_minus45 = PolarizationEllipse(0.7071068, -0.7071068, 0.0)
         self.elliptic = PolarizationEllipse(0.445, 0.89, pi/2)
+        self.circular_left_handed = PolarizationEllipse(0.7071068, 0.7071068, pi/2)
+        self.circular_right_handed = PolarizationEllipse(0.7071068, 0.7071068, -pi/2)
 
 
 if __name__ == '__main__':
