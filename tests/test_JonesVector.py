7c913bd095fbe402caf1eac6899ebc7c4f17642e
Robert Szulist <r.szulist@gmail.com>
2018-08-19 22:43:13 +0200

    Tidy up variables in tests

--- tests/test_JonesVector.py
+++ tests/test_JonesVector.py
@@ -4,28 +4,29 @@ from pylarization.vectors import JonesVector
 from tests import DummyVector
 
 
+E = sqrt(2) * 0.5
+
+
 class JonesVectorValues(DummyVector.DummyVectorValues):
     def setUp(self):
         self.linear_horizontal = JonesVector(1, 0)
         self.linear_vertical = JonesVector(0, 1)
-        a = sqrt(2) * 0.5
-        self.linear_plus45 = JonesVector(a, a)
-        self.linear_minus45 = JonesVector(a, -a)
+        self.linear_plus45 = JonesVector(E, E)
+        self.linear_minus45 = JonesVector(E, -E)
         self.elliptic = JonesVector(0.89 * 0.5, 0.89 * 1j)
-        self.circular_left_handed = JonesVector(a, -a * 1j)
-        self.circular_right_handed = JonesVector(a, a * 1j)
+        self.circular_left_handed = JonesVector(E, -E * 1j)
+        self.circular_right_handed = JonesVector(E, E * 1j)
 
 
 class JonesVectorNormalization(DummyVector.DummyVectorNormalization):
     def setUp(self):
         self.linear_horizontal = JonesVector(1, 0)
         self.linear_vertical = JonesVector(0, 1)
-        a = sqrt(2) * 0.5
-        self.linear_plus45 = JonesVector(a, a)
-        self.linear_minus45 = JonesVector(a, -a)
+        self.linear_plus45 = JonesVector(E, E)
+        self.linear_minus45 = JonesVector(E, -E)
         self.elliptic = JonesVector(0.89 * 0.5, 0.89 * 1j)
-        self.circular_left_handed = JonesVector(a, -a * 1j)
-        self.circular_right_handed = JonesVector(a, a * 1j)
+        self.circular_left_handed = JonesVector(E, -E * 1j)
+        self.circular_right_handed = JonesVector(E, E * 1j)
 
         self.linear_horizontal_normalized = JonesVector(1, 0)
         self.linear_horizontal_normalized.normalize()
@@ -33,19 +34,19 @@ class JonesVectorNormalization(DummyVector.DummyVectorNormalization):
         self.linear_vertical_normalized = JonesVector(0, 1)
         self.linear_vertical_normalized.normalize()
 
-        self.linear_plus45_normalized = JonesVector(a, a)
+        self.linear_plus45_normalized = JonesVector(E, E)
         self.linear_plus45_normalized.normalize()
 
-        self.linear_minus45_normalized = JonesVector(a, -a)
+        self.linear_minus45_normalized = JonesVector(E, -E)
         self.linear_minus45_normalized.normalize()
 
         self.elliptic_normalized = JonesVector(0.89 * 0.5, 0.89 * 1j)
         self.elliptic_normalized.normalize()
 
-        self.circular_left_handed_normalized = JonesVector(a, -a * 1j)
+        self.circular_left_handed_normalized = JonesVector(E, -E * 1j)
         self.circular_left_handed_normalized.normalize()
 
-        self.circular_right_handed_normalized = JonesVector(a, a * 1j)
+        self.circular_right_handed_normalized = JonesVector(E, E * 1j)
         self.circular_right_handed_normalized.normalize()
 
 
--- tests/test_PolarizationEllipse.py
+++ tests/test_PolarizationEllipse.py
@@ -1,18 +1,21 @@
 import unittest
-from numpy import pi
+from numpy import pi, sqrt
 from pylarization.ellipse import PolarizationEllipse
 from tests import DummyVector
+
+
+E = sqrt(2) * 0.5
 
 
 class PolarizationEllipseValues(DummyVector.DummyVectorValues):
     def setUp(self):
         self.linear_horizontal = PolarizationEllipse(1.0, 0.0, 0.0)
         self.linear_vertical = PolarizationEllipse(0.0, 1.0, 0.0)
-        self.linear_plus45 = PolarizationEllipse(0.7071068, 0.7071068, 0.0)
-        self.linear_minus45 = PolarizationEllipse(0.7071068, 0.7071068, pi)
+        self.linear_plus45 = PolarizationEllipse(E, E, 0.0)
+        self.linear_minus45 = PolarizationEllipse(E, E, pi)
         self.elliptic = PolarizationEllipse(0.445, 0.89, pi/2)
-        self.circular_left_handed = PolarizationEllipse(0.7071068, 0.7071068, -pi/2)
-        self.circular_right_handed = PolarizationEllipse(0.7071068, 0.7071068, pi/2)
+        self.circular_left_handed = PolarizationEllipse(E, E, -pi/2)
+        self.circular_right_handed = PolarizationEllipse(E, E, pi/2)
 
 
 if __name__ == '__main__':
