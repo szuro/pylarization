d7bd64541622963580b27127e6c3065c666d473b
Robert Szulist <r.szulist@gmail.com>
2018-11-01 21:36:21 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
 
 setup(
     name='pylarization',
-    version='0.1.5',
+    version='0.1.6',
     description='Package for polarization state calculations',
     long_description=long_description,
     long_description_content_type="text/x-rst",
