ffc933ec376427e4de7e1ecefe8dc7c8ad8002a3
Robert Szulist <r.szulist@gmail.com>
2019-01-19 12:17:35 +0100

    Use enums in test_StokesVector

--- tests/test_StokesVector.py
+++ tests/test_StokesVector.py
@@ -1,29 +1,30 @@
 import unittest
 from numpy import sqrt
 from pylarization.vectors import StokesVector
+from pylarization.polarizations import StokesVectorState
 from tests import DummyVector
 
 
 class TestStokesVectorValues(DummyVector.TestDummyVectorValues):
     def setUp(self):
-        self.linear_horizontal = StokesVector(1, 1, 0, 0)
-        self.linear_vertical = StokesVector(1, -1, 0, 0)
-        self.linear_plus45 = StokesVector(1, 0, 1, 0)
-        self.linear_minus45 = StokesVector(1, 0, -1, 0)
+        self.linear_horizontal = StokesVectorState.LINEAR_HORIZONTAL.value
+        self.linear_vertical = StokesVectorState.LINEAR_VERTICAL.value
+        self.linear_plus45 = StokesVectorState.LINEAR_DIAGONAL.value
+        self.linear_minus45 = StokesVectorState.LINEAR_ANTIDIAGONAL.value
+        self.circular_left_handed = StokesVectorState.CIRCULAR_LEFT_HANDED.value
+        self.circular_right_handed = StokesVectorState.CIRCULAR_RIGHT_HANDED.value
         self.elliptic = StokesVector(0.990125, -0.594075, 0, 0.7921)
-        self.circular_left_handed = StokesVector(1, 0, 0, -1)
-        self.circular_right_handed = StokesVector(1, 0, 0, 1)
 
 
 class TestStokesVectorNormalization(DummyVector.TestDummyVectorNormalization):
     def setUp(self):
-        self.linear_horizontal = StokesVector(1, 1, 0, 0)
-        self.linear_vertical = StokesVector(1, -1, 0, 0)
-        self.linear_plus45 = StokesVector(1, 0, 1, 0)
-        self.linear_minus45 = StokesVector(1, 0, -1, 0)
+        self.linear_horizontal = StokesVectorState.LINEAR_HORIZONTAL.value
+        self.linear_vertical = StokesVectorState.LINEAR_VERTICAL.value
+        self.linear_plus45 = StokesVectorState.LINEAR_DIAGONAL.value
+        self.linear_minus45 = StokesVectorState.LINEAR_ANTIDIAGONAL.value
+        self.circular_left_handed = StokesVectorState.CIRCULAR_LEFT_HANDED.value
+        self.circular_right_handed = StokesVectorState.CIRCULAR_RIGHT_HANDED.value
         self.elliptic = StokesVector(1.2371, -0.3471, 0, 0.7921)
-        self.circular_left_handed = StokesVector(1, 0, 0, -1)
-        self.circular_right_handed = StokesVector(1, 0, 0, 1)
 
         self.linear_horizontal_normalized = StokesVector(1, 1, 0, 0)
         self.linear_horizontal_normalized.normalize()
