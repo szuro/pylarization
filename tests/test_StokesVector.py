c85723767202cc812a832aab169af77eb9529722
Robert Szulist <r.szulist@gmail.com>
2019-01-30 19:20:45 +0100

    Use Enums in test_StokesVector

--- tests/test_StokesVector.py
+++ tests/test_StokesVector.py
@@ -54,32 +54,32 @@ class TestStokesVectorNormalization(DummyVector.TestDummyVectorNormalization):
         self.circular_right_handed = StokesVectorState.CIRCULAR_RIGHT_HANDED.value
         self.elliptic = StokesVector(1.2371, -0.3471, 0, 0.7921)
 
-        self.linear_horizontal_normalized = StokesVector(1, 1, 0, 0)
+        self.linear_horizontal_normalized = StokesVectorState.LINEAR_HORIZONTAL.value
         self.linear_horizontal_normalized.normalize()
 
-        self.linear_vertical_normalized = StokesVector(1, -1, 0, 0)
+        self.linear_vertical_normalized = StokesVectorState.LINEAR_VERTICAL.value
         self.linear_vertical_normalized.normalize()
 
-        self.linear_plus45_normalized = StokesVector(1, 0, 1, 0)
+        self.linear_plus45_normalized = StokesVectorState.LINEAR_DIAGONAL.value
         self.linear_plus45_normalized.normalize()
 
-        self.linear_minus45_normalized = StokesVector(1, 0, -1, 0)
+        self.linear_minus45_normalized = StokesVectorState.LINEAR_ANTIDIAGONAL.value
         self.linear_minus45_normalized.normalize()
 
         self.elliptic_normalized = StokesVector(1.2371, -0.3471, 0, 0.7921)
         self.elliptic_normalized.normalize()
 
-        self.circular_left_handed_normalized = StokesVector(1, 0, 0, -1)
+        self.circular_left_handed_normalized = StokesVectorState.CIRCULAR_LEFT_HANDED.value
         self.circular_left_handed_normalized.normalize()
 
-        self.circular_right_handed_normalized = StokesVector(1, 0, 0, 1)
+        self.circular_right_handed_normalized = StokesVectorState.CIRCULAR_RIGHT_HANDED.value
         self.circular_right_handed_normalized.normalize()
 
 
 class TestStokesVectorAddition(unittest.TestCase):
     def setUp(self):
-        self.linear_horizontal = StokesVector(1, 1, 0, 0)
-        self.linear_vertical = StokesVector(1, -1, 0, 0)
+        self.linear_horizontal = StokesVectorState.LINEAR_HORIZONTAL.value
+        self.linear_vertical = StokesVectorState.LINEAR_VERTICAL.value
 
     def test_addition(self):
         expected_sum = StokesVector(2, 0, 0, 0)
