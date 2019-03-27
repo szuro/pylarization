0c700b2a49e41ca1c67bd129f7102dbca73ff234
Robert Szulist <r.szulist@gmail.com>
2019-03-27 15:17:03 +0100

    Add test for multiplication in the wrong order

--- tests/test_JonesMatrix.py
+++ tests/test_JonesMatrix.py
@@ -52,6 +52,10 @@ class TestJonesMatrixCalculations(unittest.TestCase):
         self.assertIsInstance(result, JonesVector)
         self.assertTrue(np.allclose(result.vector, JonesVector(1, -1j).vector))
 
+    def test_vector_multiplication_with_wrong_order(self):
+        with self.assertRaises(ValueError):
+            self.vector * self.matrix
+
     def test_matrix_multiplication(self):
         result = self.matrix * self.matrix
         self.assertIsInstance(result, JonesMatrix)
